8f25bca33eb6a4b6a8ff3b5897cd37315d4cc4be
Daniel Lucas <daniellucas-pms@hotmail.com>
2021-02-08 14:22:06 -0300

    update service & tests

---
+++ src/modules/rents/services/UpdateRentService.spec.ts
@@ -0,0 +1,238 @@
+import AppError from '../../../shared/errors/AppError';
+import FakeRentsRepository from '../repositories/fakes/FakeRentsRepository';
+import FakeRentalItemsRepository from '../repositories/fakes/FakeRentalItemsRepository';
+import FakeCustomersRepository from '../../customers/repositories/fakes/FakeCustomersRepository';
+import FakeAddressCustomersRepository from '../../addresses/repositories/fakes/FakeAddressCustomersRepository';
+import FakeAddressesRepository from '../../addresses/repositories/fakes/FakeAddressesRepository';
+import FakeProductRepository from '../../products/repositories/fakes/FakeProductRepository';
+
+import CreateCustomerService from '../../customers/services/CreateCustomerService';
+import CreateAddressService from '../../addresses/services/CreateAddressService';
+import CreateProductService from '../../products/services/CreateProductService';
+import CreateRentService from './CreateRentService';
+import UpdateRentService from './UpdateRentService';
+
+let fakeAddressCustomersRepository: FakeAddressCustomersRepository;
+let fakeAddressesRepository: FakeAddressesRepository;
+let fakeCustomersRepository: FakeCustomersRepository;
+let fakeProductRepository: FakeProductRepository;
+let fakeRentsRepository: FakeRentsRepository;
+let fakeRentalItemsRepository: FakeRentalItemsRepository;
+
+let createAddress: CreateAddressService;
+let createCustomer: CreateCustomerService;
+let createProduct: CreateProductService;
+let createRent: CreateRentService;
+let updateRent: UpdateRentService;
+
+describe('UpdateRent', () => {
+  beforeEach(() => {
+    fakeAddressCustomersRepository = new FakeAddressCustomersRepository();
+    fakeAddressesRepository = new FakeAddressesRepository();
+    fakeCustomersRepository = new FakeCustomersRepository();
+    fakeProductRepository = new FakeProductRepository();
+    fakeRentsRepository = new FakeRentsRepository();
+    fakeRentalItemsRepository = new FakeRentalItemsRepository();
+
+    createAddress = new CreateAddressService(
+      fakeAddressesRepository,
+      fakeAddressCustomersRepository,
+      fakeCustomersRepository,
+    );
+
+    createCustomer = new CreateCustomerService(fakeCustomersRepository);
+    createProduct = new CreateProductService(fakeProductRepository);
+
+    createRent = new CreateRentService(
+      fakeCustomersRepository,
+      fakeAddressesRepository,
+      fakeProductRepository,
+      fakeRentsRepository,
+      fakeRentalItemsRepository,
+    );
+
+    updateRent = new UpdateRentService(
+      fakeCustomersRepository,
+      fakeAddressesRepository,
+      fakeRentsRepository,
+      fakeRentalItemsRepository,
+    );
+  });
+
+  it('should be able to update a rent', async () => {
+    const customer = await createCustomer.execute({
+      name: 'Daniel Lucas',
+      whatsapp: '12981025796',
+      cpf: '46479951867',
+    });
+
+    const address = await createAddress.execute({
+      description: 'Casa do Douglas de Souza',
+      postal_code: '12605-390',
+      city: 'Lorena',
+      neighborhood: 'Vila Passos',
+      street: 'Mario P de Aquino Filho',
+      number: '529',
+      address_type: 'Cobrança',
+    });
+
+    const product = await createProduct.execute({
+      name: 'Bolo normal',
+      quantity: 2,
+      value: 60,
+      product_type: 'Bolos',
+    });
+
+    const rent_date = new Date(2021, 1, 11);
+
+    const createdRent = await createRent.execute({
+      customer_id: customer.id,
+      address_id: address.id,
+      rent_date,
+      rental_items: [
+        {
+          product_id: product.id,
+          quantity: 1,
+          value: 60,
+        },
+      ],
+      payment_status: 'Pendente',
+      payment_way: 'Dinheiro',
+      total_value: 60,
+    });
+
+    const rent = await updateRent.execute(createdRent.id, {
+      customer_id: customer.id,
+      address_id: address.id,
+      rent_date,
+      rental_items: [
+        {
+          product_id: product.id,
+          quantity: 2,
+          value: 120,
+        },
+      ],
+      payment_status: 'Pago',
+      payment_way: 'Dinheiro',
+      total_value: 120,
+    });
+
+    expect(rent).toEqual({
+      id: rent.id,
+      rent_date,
+      total_value: 120,
+      payment_status: 'Pago',
+      payment_way: 'Dinheiro',
+      customer_id: customer.id,
+      address_id: address.id,
+    });
+  });
+
+  it('should not be able to update a nonexistent rent', async () => {
+    const customer = await createCustomer.execute({
+      name: 'Daniel Lucas',
+      whatsapp: '12981025796',
+      cpf: '46479951867',
+    });
+
+    const address = await createAddress.execute({
+      description: 'Casa do Douglas de Souza',
+      postal_code: '12605-390',
+      city: 'Lorena',
+      neighborhood: 'Vila Passos',
+      street: 'Mario P de Aquino Filho',
+      number: '529',
+      address_type: 'Cobrança',
+    });
+
+    const product = await createProduct.execute({
+      name: 'Bolo normal',
+      quantity: 1,
+      value: 60,
+      product_type: 'Bolos',
+    });
+
+    const rent_date = new Date(2021, 1, 11);
+
+    await expect(
+      updateRent.execute('nonexistent-rent-id', {
+        customer_id: customer.id,
+        address_id: address.id,
+        rent_date,
+        rental_items: [
+          {
+            product_id: product.id,
+            quantity: 2,
+            value: 120,
+          },
+        ],
+        payment_status: 'Pago',
+        payment_way: 'Dinheiro',
+        total_value: 120,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to update a rent in a past date', async () => {
+    const customer = await createCustomer.execute({
+      name: 'Daniel Lucas',
+      whatsapp: '12981025796',
+      cpf: '46479951867',
+    });
+
+    const address = await createAddress.execute({
+      description: 'Casa do Douglas de Souza',
+      postal_code: '12605-390',
+      city: 'Lorena',
+      neighborhood: 'Vila Passos',
+      street: 'Mario P de Aquino Filho',
+      number: '529',
+      address_type: 'Cobrança',
+    });
+
+    const product = await createProduct.execute({
+      name: 'Bolo normal',
+      quantity: 1,
+      value: 60,
+      product_type: 'Bolos',
+    });
+
+    const rent_date = new Date(2021, 1, 11);
+
+    const createdRent = await createRent.execute({
+      customer_id: customer.id,
+      address_id: address.id,
+      rent_date,
+      rental_items: [
+        {
+          product_id: product.id,
+          quantity: 1,
+          value: 60,
+        },
+      ],
+      payment_status: 'Pendente',
+      payment_way: 'Dinheiro',
+      total_value: 60,
+    });
+
+    const past_date = new Date(2020, 1, 11);
+
+    await expect(
+      updateRent.execute(createdRent.id, {
+        customer_id: customer.id,
+        address_id: address.id,
+        rent_date: past_date,
+        rental_items: [
+          {
+            product_id: product.id,
+            quantity: 2,
+            value: 120,
+          },
+        ],
+        payment_status: 'Pago',
+        payment_way: 'Dinheiro',
+        total_value: 120,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
--- src/modules/rents/services/UpdateRentService.ts
+++ src/modules/rents/services/UpdateRentService.ts
@@ -8,11 +8,9 @@ import Rent from '../infra/typeorm/entities/Rent';
 import IRentsRepository from '../repositories/IRentsRepository';
 import IRentalItemsRepository from '../repositories/IRentalItemsRepository';
 import ICustomersRepository from '../../customers/repositories/ICustomersRepository';
-import IProductsRepository from '../../products/repositories/IProductsRepository';
 import IAddressesRepository from '../../addresses/repositories/IAddressesRepository';
 
 import { RentItem } from '../dtos/ICreateRentDTO';
-import RentalItem from '../infra/typeorm/entities/RentalItem';
 
 interface Request {
   customer_id: string;
@@ -32,9 +30,6 @@ class UpdateRentService {
 
     @inject('AddressesRepository')
     private addressesRepository: IAddressesRepository,
-
-    @inject('ProductsRepository')
-    private productsRepository: IProductsRepository,
 
     @inject('RentsRepository')
     private rentsRepository: IRentsRepository,
@@ -61,86 +56,8 @@ class UpdateRentService {
       throw new AppError("Rent doesn't exist.", 400);
     }
 
-    const customerExists = await this.customersRepository.findById(customer_id);
-
-    if (!customerExists) {
-      throw new AppError("Customer doesn't exist.", 400);
-    }
-
-    const addressExists = await this.addressesRepository.findById(address_id);
-
-    if (!addressExists) {
-      throw new AppError("Address doesn't exist.", 400);
-    }
-
     if (isBefore(rent_date, Date.now())) {
       throw new AppError("You can't create a rent in a past date");
-    }
-
-    const productsIDs = rental_items.map(item => item.product_id);
-
-    const rentedProducts = await this.productsRepository.findByIds(productsIDs);
-
-    for (let i = 0; i < rental_items.length; i++) {
-      const product = rentedProducts.find(item => {
-        return item.id === rental_items[i].product_id;
-      });
-      if (product) {
-        if (rental_items[i].quantity > product.quantity) {
-          throw new AppError('Not enough products', 400);
-        }
-      }
-    }
-
-    const rentsInThisDay = await this.rentsRepository.findByDate(rent_date);
-
-    if (rentsInThisDay.length !== 0) {
-      const rentsInThisDayIDs = rentsInThisDay.map(rent => rent.id);
-
-      const rentedItemsInThisDay = await this.rentalItemsRepository.findByRents(
-        rentsInThisDayIDs,
-      );
-
-      const consolidatedRentedItemsInThisDay: RentalItem[] = [];
-
-      rentedItemsInThisDay.forEach(rentedItem => {
-        const itemIndex = consolidatedRentedItemsInThisDay.findIndex(
-          item => item.product_id === rentedItem.product_id,
-        );
-
-        if (itemIndex !== -1) {
-          consolidatedRentedItemsInThisDay[itemIndex].quantity +=
-            rentedItem.quantity;
-
-          consolidatedRentedItemsInThisDay[itemIndex].value += rentedItem.value;
-        } else {
-          consolidatedRentedItemsInThisDay.push(rentedItem);
-        }
-      });
-
-      consolidatedRentedItemsInThisDay.forEach(consolidatedRentedItem => {
-        rental_items.forEach(rentalItem => {
-          if (consolidatedRentedItem.product_id === rentalItem.product_id) {
-            const product = rentedProducts.find(
-              item => item.id === consolidatedRentedItem.product_id,
-            );
-
-            if (product) {
-              // A quantidade alugada nessse dia é igual a disponível em estoque?
-              if (consolidatedRentedItem.quantity === product.quantity) {
-                throw new AppError('All units of this product are rented', 400);
-              }
-
-              const quantityAvailableInStock =
-                product.quantity - consolidatedRentedItem.quantity;
-
-              if (rentalItem.quantity > quantityAvailableInStock) {
-                throw new AppError('Not enough products', 400);
-              }
-            }
-          }
-        });
-      });
     }
 
     const rentedItems = await this.rentalItemsRepository.findByRent(
