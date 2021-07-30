4814e69a8b8e77d75915211e6dba2e05da7c41be
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2021-07-30 15:19:39 -0300

    UsersRoles enum

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import authRole from '../../../../../shared/infra/http/middlewares/authRole';
+import { UsersRoles } from '../../typeorm/entities/User';
 import UsersController from '../controllers/UsersController';
 
 const usersRouter = Router();
 
 const usersController = new UsersController();
 
-usersRouter.use(authRole('ADM'));
+usersRouter.use(authRole(UsersRoles.ADM));
 usersRouter.post('/', usersController.create);
 usersRouter.get('/', usersController.index);
 usersRouter.get('/:id', usersController.show);
