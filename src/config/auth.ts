bfe47c6107581d2da352f588cc591ade1b5c9494
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2021-08-05 15:06:31 -0300

    refactor: env vars + dotenv

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: '7e4dc317d93cb7296f2505b44ac782fe',
-    expiresIn: '1d',
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES_IN,
   },
 };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import { app } from './app';
 
-const port = process.env.port || 3333;
+const port = process.env.APP_PORT || 3333;
 
 app.listen(port, () => console.log(`Server listen on port ${port}`));
