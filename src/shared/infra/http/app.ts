fa165e59b26389a7694bb4a35417d8040b3d88d2
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2021-08-14 23:45:52 -0300

    docs: insomnia docs

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
 import 'reflect-metadata';
+import http from 'http';
+import path from 'path';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import createConnection from '../typeorm';
@@ -8,27 +10,35 @@ import routes from './routes';
 import AppError from '../../errors/AppError';
 import rateLimiter from './middlewares/rateLimiter';
 
-const app = express();
+const expressApp = express();
 createConnection();
+const app = http.createServer(expressApp);
 
-app.use(rateLimiter);
-app.use(express.json());
-app.use(routes);
+expressApp.use(
+  express.static(
+    path.resolve(__dirname, '..', '..', '..', '..', 'public', 'docs'),
+  ),
+);
+expressApp.use(rateLimiter);
+expressApp.use(express.json());
+expressApp.use(routes);
 
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
+expressApp.use(
+  (err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.log(err);
+
+    return response.status(500).json({
       status: 'error',
-      message: err.message,
+      message: 'Internal server error',
     });
-  }
-
-  console.log(err);
-
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-});
+  },
+);
 
 export { app };
